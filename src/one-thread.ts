a1d1f0edd01c13785f387eb79a7850d0eaa53799
Josh Wulf <josh@magikcraft.io>
2021-02-26 12:09:28 +1300

    Update
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/one-thread.ts
+++ src/one-thread.ts
@@ -8,12 +8,15 @@ const workflows = [
 	'./bpmn/uncontested-with-decisions.bpmn',
 ]
 
+const p: number[] = []
+
 zbc.createWorker(null, 'task1', (_, complete) => {
 	const w1 = Date.now()
 	complete
 		.success({ w1 })
 		.then(() => console.log(`${Date.now() - w1}ms to broker ack complete 1`))
-	console.log(`${Date.now() - w1}ms in handler 1`)
+	p.push(Date.now() - w1)
+	// console.log(`${p[0]}ms in handler 1`)
 })
 
 zbc.createWorker(null, 'task2', (_, complete) => {
@@ -21,7 +24,8 @@ zbc.createWorker(null, 'task2', (_, complete) => {
 	complete
 		.success({ w2 })
 		.then(() => console.log(`${Date.now() - w2}ms to broker ack complete 2`))
-	console.log(`${Date.now() - w2}ms in handler 2`)
+	p.push(Date.now() - w2)
+	// console.log(`${p[1]}ms in handler 2`)
 })
 
 zbc.createWorker(null, 'task3', (_, complete) => {
@@ -29,7 +33,8 @@ zbc.createWorker(null, 'task3', (_, complete) => {
 	complete
 		.success({ w3 })
 		.then(() => console.log(`${Date.now() - w3}ms to broker ack complete 3`))
-	console.log(`${Date.now() - w3}ms in handler 3`)
+	p.push(Date.now() - w3)
+	// console.log(`${p[2]}ms in handler 3`)
 })
 
 zbc.createWorker(null, 'task4', (_, complete) => {
@@ -37,7 +42,8 @@ zbc.createWorker(null, 'task4', (_, complete) => {
 	complete
 		.success({ w4 })
 		.then(() => console.log(`${Date.now() - w4}ms to broker ack complete 4`))
-	console.log(`${Date.now() - w4}ms in handler 4`)
+	p.push(Date.now() - w4)
+	// console.log(`${p[3]}ms in handler 4`)
 })
 
 zbc.createWorker(null, 'task5', (_, complete) => {
@@ -45,22 +51,25 @@ zbc.createWorker(null, 'task5', (_, complete) => {
 	complete
 		.success({ w5 })
 		.then(() => console.log(`${Date.now() - w5}ms to broker ack complete 5`))
-	console.log(`${Date.now() - w5}ms in handler 5`)
+	p.push(Date.now() - w5)
+	// console.log(`${p[4]}ms in handler 5`)
 })
 
 async function main() {
 	// Deploy workflows
 	await zbc.deployWorkflow(workflows[0])
+	console.log('\n== Communication latency ==')
 	const start = Date.now()
 	const res = await zbc.createWorkflowInstanceWithResult('uncontested', { start })
 	const end = Date.now()
-	console.log({ end })
-	console.log(`Total time: ${end - start}ms`)
-	console.log(`Task 1: ${res.variables.w1 - start}ms`)
-	console.log(`Task 2: ${res.variables.w2 - res.variables.w1}ms`)
-	console.log(`Task 3: ${res.variables.w3 - res.variables.w2}ms`)
-	console.log(`Task 4: ${res.variables.w4 - res.variables.w3}ms`)
-	console.log(`Task 5: ${res.variables.w5 - res.variables.w4}ms`)
+	// console.log({ end })
+	console.log(`\nEnd-to-end time: ${end - start}ms`)
+	console.log('\n== Broker overhead ==')
+	console.log(`Start -> Task 1 start: ${res.variables.w1 - start}ms`)
+	console.log(`Task 1 end -> Task 2 start: ${res.variables.w2 - res.variables.w1 - p[0]}ms`)
+	console.log(`Task 2 end -> Task 3 start: ${res.variables.w3 - res.variables.w2 - p[1]}ms`)
+	console.log(`Task 3 end -> Task 4 start: ${res.variables.w4 - res.variables.w3 - p[2]}ms`)
+	console.log(`Task 4 end -> Task 5 start: ${res.variables.w5 - res.variables.w4 - p[3]}ms`)
 }
 
 main()
